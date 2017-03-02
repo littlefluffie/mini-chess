6e5728db8a33f484ac4931f9ac7d7933735263fa
Donovan Jordaan <donovan@journeyapps.com>
2017-03-02 18:04:05 +0200

    En Passant, Pawn movement and capturing

--- chess.js
+++ chess.js
@@ -1,11 +1,11 @@
 var board = "1010101001010101101010100101010110101010010101011010101001010101";
-var default_pieces = "RNBQKBNRPPPPPPPP00000000000000000000000000000000pppppppprnbqkbnr";
+var default_pieces = "RNBQKBNROOOOOOOO00000000000000000000000000000000oooooooornbqkbnr";
 // var default_pieces = "000000000000000000000000000n000000000000000000000000000000000000";
 
 var pieces = Array.from(default_pieces);
 var files = "abcdefgh";
-var white = "kqrbnp";
-var black = "KQRBNP";
+var white = "kqrbnopz";
+var black = "KQRBNOPZ";
 
 var chess_set = {
     "k": String.fromCharCode(9812),
@@ -13,13 +13,18 @@ var chess_set = {
     "r": String.fromCharCode(9814),
     "b": String.fromCharCode(9815),
     "n": String.fromCharCode(9816),
-    "p": String.fromCharCode(9817),
+    "o": String.fromCharCode(9817), // Initial pawn
+    "p": String.fromCharCode(9817), // Moved pawn
+    "z": "", // En Passant
+
     "K": String.fromCharCode(9818),
     "Q": String.fromCharCode(9819),
     "R": String.fromCharCode(9820),
     "B": String.fromCharCode(9821),
     "N": String.fromCharCode(9822),
-    "P": String.fromCharCode(9823)
+    "O": String.fromCharCode(9823), // Initial pawn
+    "P": String.fromCharCode(9823), // Moved pawn
+    "Z": "" // En Passant
 }
 
 function toIndex(notation) {
@@ -29,17 +34,21 @@ function toIndex(notation) {
 }
 
 function movePiece(piece_index, square_index) {
+    pieces = pieces.map(function(a) {
+        if ((a === "z") || (a === "Z")) return "0";
+        else return a;
+    });
+
     var piece = pieces[piece_index];
     pieces[piece_index] = "0";
-    pieces[square_index] = piece;
-    return displayBoard();
-}
-
-function getLegalMoves(index) {
-    var piece = pieces[index];
-    if (piece !== "0") {
-
+    if (piece === "o" || piece === "O") {
+        pieces[square_index] = (piece === "o") ? "p" : "P";
+        pieces[square_index - (-1 + 2 * color(piece)) * 8] = (piece === "o") ? "z" : "Z";
+    } else {
+        pieces[square_index] = piece;
     }
+
+    return displayBoard();
 }
 
 function color(piece) {
@@ -56,14 +65,18 @@ function getPossibleMoves(index) {
     var row = Math.floor(index / 8);
     var column = index % 8;
 
-    function vector(distance) {
-        for (var n = 1; n < distance; n++) {
+    function vector(distance, capture) {
+        for (var n = 1; n < distance + 1; n++) {
             if (row + (dy * n) >= 0 && row + (dy * n) < 8 && column + (dx * n) >= 0 && column + (dx * n) < 8) {
                 var move = (row + (dy * n)) * 8 + column + (dx * n);
                 if (pieces[move] === "0") {
+                    if ("pPoO".includes(piece) && capture) return;
+                    else moves.push(move);
+                } else if ((pieces[move] === "z" || pieces[move] === "Z") && color(piece) === 1 - color(pieces[move])) {
                     moves.push(move);
+                    pieces[move + (1 - 2 * color(piece)) * 8] = "0";
                 } else {
-                    if (color(piece) === 1 - color(pieces[move])) moves.push(move);
+                    if (capture && color(piece) === 1 - color(pieces[move])) moves.push(move);
                     return;
                 }
             } else {
@@ -78,7 +91,7 @@ function getPossibleMoves(index) {
             case "K":
                 for (var dx = -1; dx < 2; dx++) {
                     for (var dy = -1; dy < 2; dy++) {
-                        vector(1);
+                        vector(1, true);
                     }
                 }
                 break;
@@ -87,7 +100,7 @@ function getPossibleMoves(index) {
             case "Q":
                 for (var dx = -1; dx < 2; dx++) {
                     for (var dy = -1; dy < 2; dy++) {
-                        vector(9);
+                        vector(8, true);
                     }
                 }
                 break;
@@ -97,7 +110,7 @@ function getPossibleMoves(index) {
                 for (var i = 0; i < 4; i++) {
                     var dx = Math.cos(Math.PI / 2 * i);
                     var dy = Math.sin(Math.PI / 2 * i);
-                    vector(9);
+                    vector(8, true);
                 }
                 break;
 
@@ -128,21 +141,42 @@ function getPossibleMoves(index) {
             case "B":
                 for (var dx = -1; dx < 2; dx += 2) {
                     for (var dy = -1; dy < 2; dy += 2) {
-                        vector(9);
+                        vector(9, true);
                     }
                 }
                 break;
 
+            case "o":
+            case "O":
+                var i = -1 + 2 * color(piece);
+                var dx = Math.floor(Math.cos(Math.PI / 2 * i));
+                var dy = Math.floor(Math.sin(Math.PI / 2 * i));
+                vector(2, false);
+
+                var dy = i;
+
+                var dx = 1;
+                vector(1, true);
+
+                var dx = -1;
+                vector(1, true);
+
+                break;
+
             case "p":
             case "P":
-                var move = (row - 1) * 8 + column;
-                if (pieces[move] === "0") moves.push(move);
-                var move = (row - 2) * 8 + column;
-                if (pieces[move] === "0") moves.push(move);
-                var move = (row - 1) * 8 + column + 1;
-                if (pieces[move] !== "0" && color(pieces[move] === 1 - color(piece))) moves.push(move);
-                var move = (row - 1) * 8 + column - 1;
-                if (pieces[move] !== "0" && color(pieces[move] === 1 - color(piece))) moves.push(move);
+                var i = -1 + 2 * color(piece);
+                var dx = 0;
+                var dy = Math.floor(Math.sin(Math.PI / 2 * i));
+                vector(1, false);
+
+                var dy = i;
+                var dx = 1;
+                vector(1, true);
+
+                var dx = -1;
+                vector(1, true);
+
                 break;
 
             default:
